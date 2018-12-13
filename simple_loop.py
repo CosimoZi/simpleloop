e15849b1953ebdbd7e46b5ef07dff84ba4b5ed11
CosimoZi <xzyufo@gmail.com>
2018-12-13 10:53:37 +0800

    Finish 0.4

--- simple_loop.py
+++ simple_loop.py
@@ -1,5 +1,10 @@
 from types import coroutine
 from collections import defaultdict
+from heapq import heappop, heappush
+from timeit import default_timer as now
+from time import sleep as _sleep
+
+from timer import timer
 
 
 @coroutine
@@ -13,16 +18,22 @@ def hello_world():
 
 
 class Event:
+    pass
+
+
+class SpawnEvent(Event):
     def __init__(self, coro):
         self.coro = coro
 
 
-class SpawnEvent(Event):
-    pass
-
-
 class JoinEvent(Event):
-    pass
+    def __init__(self, coro):
+        self.coro = coro
+
+
+class SleepEvent(Event):
+    def __init__(self, seconds):
+        self.wakeup_time = seconds + now()
 
 
 @coroutine
@@ -37,23 +48,42 @@ def join(future):
     return result
 
 
+@coroutine
+def sleep(seconds):
+    yield SleepEvent(seconds)
+
+
 async def main_coro():
     future = await spawn(hello_world())
     result = await join(future)
+    with timer():
+        await sleep(3)
+    with timer():
+        await sleep(2)
     return result
 
 
 class Task:
-    def __init__(self, coro, trigger):
+    def __init__(self, coro, trigger, wakeup_time=None):
         self.coro = coro
         self.trigger = trigger
+        self.wakeup_time = wakeup_time if wakeup_time else now()
+
+    def __cmp__(self, other):
+        return self.wakeup_time - other.wakeup_time
 
 
 def run_until_complete(coro):
     tasks = [Task(coro, None)]
     watcher = defaultdict(list)
+    delayed_tasks = []
     ret = None
-    while tasks:
+    while tasks or delayed_tasks:
+        if not tasks:
+            _sleep(max((0.0, delayed_tasks[0].wakeup_time - now())))
+        while delayed_tasks and delayed_tasks[0].wakeup_time < now():
+            task = heappop(delayed_tasks)
+            tasks.append(task)
         queue, tasks = tasks, []
         for task in queue:
             try:
@@ -67,10 +97,13 @@ def run_until_complete(coro):
                     tasks.append(Task(task.coro, res.coro))
                 elif isinstance(res, JoinEvent):
                     watcher[res.coro].append(task.coro)
+                elif isinstance(res, SleepEvent):
+                    heappush(delayed_tasks, Task(task.coro, None, wakeup_time=res.wakeup_time))
                 else:
                     tasks.append(task)
     return ret
 
 
 if __name__ == '__main__':
-    print(run_until_complete(main_coro()))
+    with timer():
+        print(run_until_complete(main_coro()))
